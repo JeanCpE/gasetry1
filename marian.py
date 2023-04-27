2d06e2d0881a9ad84489ebe144a4b0dfdf7bb6a7
gaseapp <131535479+gaseapp@users.noreply.github.com>
2023-04-27 10:58:37 +0800

    Update marian.py

--- marian.py
+++ marian.py
@@ -11,7 +11,7 @@ layout = "centered"
 # Everything is accessible via the st.secrets dict:
 st.write("DB username:", st.secrets["Marian"])
 st.write("DB password:", st.secrets["Marian12345"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
 # And the root-level secrets are also accessible as environment variables:
 
 import os
