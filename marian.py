0d2a2681f200153dd98a0a8179b220df59ee1932
Marian <medatup@gmail.com>
2023-04-29 20:51:46 +0800

    Program updated. Map using leafmap was updated. CSV file reading not working.

--- marian.py
+++ marian.py
@@ -32,12 +32,6 @@ else:
 
 #-+++++++++++++++++++++++++++++++++++++++
 #----------------------------------------
-
-# streamlit_app.py
-
-import pandas as pd
-import streamlit as st
-
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
